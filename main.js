fd229c7809c7b0cbe34738bb14f091224d1090c0
Gerry Blackmon <blackmongerry@gmail.com>
2018-08-16 16:30:15 -0700

    changed movement to directional

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 $(document).ready(initializeGame)
 var playerTank
+var shotsFired = [];
 
 function initializeGame(){
-    playerTank = new Tank( {img: 'images/tankPic.jpeg', xPosition: 0, yPosition: 25} );
+    playerTank = new Tank( {img: 'images/tankPic.jpeg', xPosition: 0, yPosition: 25, angleOfDirection: 0} );
     attachEventHandlers();
 }
 
@@ -12,13 +13,13 @@ function attachEventHandlers(){
 function userKeyPresses(event){
     switch(event.which){
         case 37:
-            playerTank.moveLeft();
+            playerTank.turnLeft();
             break;
         case 38: 
-            playerTank.moveUp();
+            playerTank.moveForward(playerTank.selector);
             break;
         case 39:
-            playerTank.moveRight();
+            playerTank.turnRight();
             break;
         case 40:
             playerTank.moveDown();
@@ -29,11 +30,24 @@ function userKeyPresses(event){
     }
 }
 
-class Tank{
+class ScreenObjects{
+    constructor( screenObjectOptions ){
+        this.img = screenObjectOptions.img;
+        this.xPosition = screenObjectOptions.xPosition;
+        this.yPosition = screenObjectOptions.yPosition;
+        this.angleOfDirection = screenObjectOptions.angleOfDirection;
+    }
+    moveForward(selector){
+        this.xPosition = Math.cos(this.angleOfDirection) * 10;
+        this.yPosition = Math.sin(this.angleOfDirection) * 10;
+        this.configObj['css']['left'] = this.xPosition + '%';
+        this.configObj['css']['top'] = this.yPosition + '%';
+        selector.css(this.configObj['css']);
+    }
+}
+class Tank extends ScreenObjects{
     constructor( tankOptions ){
-        this.img = tankOptions.img;
-        this.xPosition = tankOptions.xPosition;
-        this.yPosition = tankOptions.yPosition;
+        super(tankOptions);
         this.randomID = 'tank' + Math.floor(Math.random() * 1000);
         this.configObj = {'class': 'tank', 
                             id: this.randomID,
@@ -46,46 +60,67 @@ class Tank{
     }
     shoot(){
         console.log('BANG!');
-        debugger;
-        var cannonShot = $('<img>', {src: 'images/cannonBall.jpg',
-                                        'class': 'cannonBall',
-                                        css: {left: this.xPosition + '%',
-                                                top: this.yPosition +'%'}
-                                        } )
-        $('#mainScreen').append(cannonShot);
+        shotsFired.push(new CannonBall( {xPosition: this.xPosition, 
+                                        yPosition: this.yPosition, 
+                                        img: 'images/cannonBall.jpg',} ));
+        console.log(shotsFired[0])
     }
-    moveLeft(){
-        this.xPosition -= 1.25;
-        this.configObj['css']['left'] = this.xPosition+'%';
-        this.selector.css(this.configObj['css']);
-        
-    }
-    moveRight(){
-        this.xPosition += 1.25;
-        this.configObj['css']['left'] = this.xPosition+'%';
+    turnLeft(){
+        this.angleOfDirection -= 10;
+        this.configObj['css']['transform'] = 'rotate('+this.angleOfDirection+'deg)';
         this.selector.css(this.configObj['css']);
     }
-    moveUp(){
-        this.yPosition -= 1.25;
-        this.configObj['css']['top'] = this.yPosition+'%';
+    turnRight(){
+        this.angleOfDirection += 10;
+        this.configObj['css']['transform'] = 'rotate('+this.angleOfDirection+'deg)';
         this.selector.css(this.configObj['css']);
     }
-    moveDown(){
-        this.yPosition += 1.25;
-        this.configObj['css']['top'] = this.yPosition+'%';
-        this.selector.css(this.configObj['css']);
-    }
-    explode(){
+    moveReverse(){
 
     }
+    // moveLeft(){
+    //     this.xPosition -= 1.25;
+    //     this.configObj['css']['left'] = this.xPosition+'%';
+    //     this.selector.css(this.configObj['css']);
+        
+    // }
+    // moveRight(){
+    //     this.xPosition += 1.25;
+    //     this.configObj['css']['left'] = this.xPosition+'%';
+    //     this.selector.css(this.configObj['css']);
+    // }
+    // moveUp(){
+    //     this.yPosition -= 1.25;
+    //     this.configObj['css']['top'] = this.yPosition+'%';
+    //     this.selector.css(this.configObj['css']);
+    // }
+    // moveDown(){
+    //     this.yPosition += 1.25;
+    //     this.configObj['css']['top'] = this.yPosition+'%';
+    //     this.selector.css(this.configObj['css']);
+    // }
+
 }
-class CannonBall{
+
+
+
+class CannonBall extends ScreenObjects{
     constructor( cannonBallOptions ){
-        this.xPosition = cannonBallOptions.xPosition;
-        this.yPosition = cannonBallOptions.yPosition;
-        this.configObj = {left: this.xPosition + '%',
-                            top: this.yPosition +'%'}
-        //this.ball = $('<img>', )
+        super(cannonBallOptions);
+        this.randomID = 'cannonball' + Math.floor(Math.random() * 1000);
+        this.configObj = {'class': 'cannonBall',
+                            id: this.randomID,
+                            src: this.img,
+                            css: {'left': this.xPosition + '%',
+                                    'top': this.yPosition +'%'},
+                            alt: 'cannon ball img'
+                        }
+        this.ball = $('<img>', this.configObj);
+        $('#mainScreen').append(this.ball);
+        this.selector = $('#'+this.randomID);
+    }
+    movement(){
+        
     }
 }
 
@@ -107,3 +142,68 @@ class Sound{
         this.sound.pause();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Tank{
+//     constructor( tankOptions ){
+//         this.img = tankOptions.img;
+//         this.xPosition = tankOptions.xPosition;
+//         this.yPosition = tankOptions.yPosition;
+//         this.randomID = 'tank' + Math.floor(Math.random() * 1000);
+//         this.configObj = {'class': 'tank', 
+//                             id: this.randomID,
+//                             src: this.img,
+//                             css: {'top': this.yPosition+'%', left: this.xPosition+'%'},
+//                             alt: 'tank Img'}
+//         this.tank = $('<img>', this.configObj);
+//         $('#mainScreen').append(this.tank);
+//         this.selector = $('#'+this.randomID);
+//     }
+//     shoot(){
+//         console.log('BANG!');
+//         shotsFired.push(new CannonBall( {xPosition: this.xPosition, yPosition: this.yPosition} ));
+//         console.log(shotsFired[0])
+//     }
+//     moveLeft(){
+//         this.xPosition -= 1.25;
+//         this.configObj['css']['left'] = this.xPosition+'%';
+//         this.selector.css(this.configObj['css']);
+        
+//     }
+//     moveRight(){
+//         this.xPosition += 1.25;
+//         this.configObj['css']['left'] = this.xPosition+'%';
+//         this.selector.css(this.configObj['css']);
+//     }
+//     moveUp(){
+//         this.yPosition -= 1.25;
+//         this.configObj['css']['top'] = this.yPosition+'%';
+//         this.selector.css(this.configObj['css']);
+//     }
+//     moveDown(){
+//         this.yPosition += 1.25;
+//         this.configObj['css']['top'] = this.yPosition+'%';
+//         this.selector.css(this.configObj['css']);
+//     }
+//     explode(){
+
+//     }
+// }
