2ebae3d7c366efe3c9d11251ef37766a7b7894f0
Gerry Blackmon <blackmongerry@gmail.com>
2018-08-28 15:18:36 -0700

    changing move forward function

--- classes/screenObjects.js
+++ classes/screenObjects.js
@@ -6,11 +6,40 @@ class ScreenObjects{
         this.angleOfDirection = screenObjectOptions.angleOfDirection;
     }
     moveForward( selector ){
-        // var newXPosition = Math.cos( this.angleOfDirection ) * 10;
-        // this.xPosition += newXPosition;
-        // var newYPosition = Math.sin( this.angleOfDirection ) * 10;
+        var newXPosition = Math.sin( this.angleOfDirection ) * 10;
+        var newYPosition = Math.cos( this.angleOfDirection ) * 10;
+        console.log('newXPosition',newXPosition)
+        console.log('newYPosition',newYPosition)
 
-        
+        if( this.angleOfDirection <= 90 ){
+            console.log('current x pos:', this.xPosition);
+            this.xPosition += newXPosition;
+            this.yPosition -= newYPosition;
+            console.log('new x pos:', this.xPosition);
+        }
+        else if( this.angleOfDirection <= 180 ){
+            console.log('current x pos:', this.xPosition);
+
+            this.xPosition += newXPosition;
+            this.yPosition += newYPosition;
+            console.log('new x pos:', this.xPosition);
+
+        }
+        else if( this.angleOfDirection <= 270 ){
+            console.log('current x pos:', this.xPosition);
+
+            this.xPosition -= newXPosition;
+            this.yPosition += newYPosition;
+            console.log('new x pos:', this.xPosition);
+
+        }
+        else{
+            console.log('current x pos:', this.xPosition);
+            this.xPosition -= newXPosition;
+            this.yPosition -= newYPosition;
+            console.log('new x pos:', this.xPosition);
+
+        }
         this.configObj[ 'css' ][ 'left' ] = this.xPosition + '%';
         this.configObj[ 'css' ][ 'top' ] = this.yPosition + '%';
         selector.css( this.configObj[ 'css' ] );
