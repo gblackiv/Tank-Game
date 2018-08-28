8035f5fce04478c9e6ee41ceae8db19412d8ce7e
Gerry Blackmon <blackmongerry@gmail.com>
2018-08-28 14:05:36 -0700

    working on math for moveForward

--- classes/screenObjects.js
+++ classes/screenObjects.js
@@ -6,8 +6,11 @@ class ScreenObjects{
         this.angleOfDirection = screenObjectOptions.angleOfDirection;
     }
     moveForward( selector ){
-        this.xPosition = Math.cos( this.angleOfDirection ) * 10;
-        this.yPosition = Math.sin( this.angleOfDirection ) * 10;
+        // var newXPosition = Math.cos( this.angleOfDirection ) * 10;
+        // this.xPosition += newXPosition;
+        // var newYPosition = Math.sin( this.angleOfDirection ) * 10;
+
+        
         this.configObj[ 'css' ][ 'left' ] = this.xPosition + '%';
         this.configObj[ 'css' ][ 'top' ] = this.yPosition + '%';
         selector.css( this.configObj[ 'css' ] );
