cc0e649e23456cdbc8267d720262163fbc20ee04
gblackiv <blackmongerry@gmail.com>
2018-10-31 12:34:59 -0700

    tank now circles player

--- classes/tank_bot.js
+++ classes/tank_bot.js
@@ -7,20 +7,30 @@ class TankBot extends Tank{
 		this.getHitBox();
 		this.collisionDetection( this.currentGameBoard.shotsFiredArray );
 		if( this.amITurningLeft ){
-            this.turnLeft();
-        }
-        if( this.amITurningRight ){
-            this.turnRight();
-        }
-		this.shoot();
+			this.turnLeft();
+		}
+		if( this.amITurningRight ){
+			this.turnRight();
+		}
+		this.moveForward();
 	}
 	scan(){
 		let deltaX = -(this.hitBox.x - this.currentGameBoard.playerTank.hitBox.x);
 		let deltaY = -(this.hitBox.y - this.currentGameBoard.playerTank.hitBox.y);
-		let theta = ( Math.atan2( deltaY, deltaX ) * degreeConversionFactor ) + 90;
-		this.facePlayerTank( theta );
+		let theta = ( Math.atan2( deltaY, deltaX ) * degreeConversionFactor );
+		let pythagoreanDistance = Math.sqrt( Math.pow( deltaX, 2 ) + Math.pow( deltaY, 2 ) );
+		console.log(pythagoreanDistance);
+		if( tankBotCircleRange + 50 > pythagoreanDistance && pythagoreanDistance > tankBotCircleRange ){
+			this.changeFacing( theta + 180 );
+		}
+		else if( pythagoreanDistance < tankBotCircleRange ){
+			this.changeFacing( theta );
+		}
+		else{
+			this.changeFacing( theta + 90 );
+		}
 	}
-	facePlayerTank( theta ){
+	changeFacing( theta ){
 		console.log(this.angleOfDirection)
 		if( this.angleOfDirection + 5 > theta ){
 			this.toggleTurningLeftOn();
