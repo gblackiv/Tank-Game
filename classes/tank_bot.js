8208bb18caafadc535f6a78df3f32af4ee6721c7
gblackiv <blackmongerry@gmail.com>
2018-10-30 18:28:59 -0700

    fixing the turning of the ai tank

--- classes/tank_bot.js
+++ classes/tank_bot.js
@@ -3,8 +3,33 @@ class TankBot extends Tank{
 		super( tankConfigObj );
 	}
 	handleAIHeartbeat(){
+		this.scan();
 		this.getHitBox();
 		this.collisionDetection( this.currentGameBoard.shotsFiredArray );
+		if( this.amITurningLeft ){
+            this.turnLeft();
+        }
+        if( this.amITurningRight ){
+            this.turnRight();
+        }
 		this.shoot();
+	}
+	scan(){
+		let deltaX = -(this.hitBox.x - this.currentGameBoard.playerTank.hitBox.x);
+		let deltaY = -(this.hitBox.y - this.currentGameBoard.playerTank.hitBox.y);
+		let theta = ( Math.atan2( deltaY, deltaX ) * degreeConversionFactor ) + 90;
+		console.log(theta)
+		if( this.angleOfDirection > theta ){
+			this.toggleTurningLeftOn();
+		}
+		else{
+			this.toggleTurningLeftOff();
+		}
+		if( this.angleOfDirection < theta ){
+			this.toggleTurningRightOn();
+		}
+		else{
+			this.toggleTurningRightOff();
+		}
 	}
 }
