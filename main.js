728cdca3a6480cf6834282ebfd43f70014ff62cb
gblackiv <blackmongerry@gmail.com>
2018-11-14 15:24:15 -0800

    added try catch blocks around handlers that require the player to be alive

--- main.js
+++ main.js
@@ -20,61 +20,68 @@ function modalClose( event ){
     event.stopPropagation()
 }
 function shootWithMouse( event ){
-    if( typeof theGameScreen === 'undefined' || typeof theGameScreen.playerTank === 'undefined' || !theGameScreen.playerTank ){
+    try{
+        theGameScreen.playerTank.shoot()
+    }
+    catch{
         return;
     }
-    theGameScreen.playerTank.shoot()
 }
 function userKeyDownPresses( event ){
-    if( typeof theGameScreen === 'undefined' || typeof theGameScreen.playerTank === 'undefined' || !theGameScreen.playerTank ){
-        return;
+    try{
+        switch( event.which ){
+            case 65:
+                theGameScreen.playerTank.toggleTurningLeftOn();
+                break;
+            case 87: 
+                theGameScreen.playerTank.toggleForwardMovementOn();
+                break;
+            case 83:
+                theGameScreen.playerTank.toggleReverseMovementOn();
+                break;
+            case 68:
+                theGameScreen.playerTank.toggleTurningRightOn();
+                break;
+            case 32: 
+                theGameScreen.playerTank.shoot();
+                break;
+        }
     }
-    switch( event.which ){
-        case 65:
-            theGameScreen.playerTank.toggleTurningLeftOn();
-            break;
-        case 87: 
-            theGameScreen.playerTank.toggleForwardMovementOn();
-            break;
-        case 83:
-            theGameScreen.playerTank.toggleReverseMovementOn();
-            break;
-        case 68:
-            theGameScreen.playerTank.toggleTurningRightOn();
-            break;
-        case 32: 
-            theGameScreen.playerTank.shoot();
-            break;
+    catch{
+        return;
     }
 }
 function userKeyUpPresses( event ){
-    if( typeof theGameScreen === 'undefined' || typeof theGameScreen.playerTank === 'undefined' || !theGameScreen.playerTank ){
-        return;
+    try{
+        switch( event.which ){
+            case 65:
+                theGameScreen.playerTank.toggleTurningLeftOff();
+                break;
+            case 87:
+                theGameScreen.playerTank.toggleForwardMovementOff();
+                break;
+            case 83:
+                theGameScreen.playerTank.toggleReverseMovementOff();
+                break;
+            case 68:
+                theGameScreen.playerTank.toggleTurningRightOff();
+                break;
+        }
     }
-    switch( event.which ){
-        case 65:
-            theGameScreen.playerTank.toggleTurningLeftOff();
-            break;
-        case 87:
-            theGameScreen.playerTank.toggleForwardMovementOff();
-            break;
-        case 83:
-            theGameScreen.playerTank.toggleReverseMovementOff();
-            break;
-        case 68:
-            theGameScreen.playerTank.toggleTurningRightOff();
-            break;
+    catch{
+        return;
     }
 }
 function mouseMoving( event ){
-    if( typeof theGameScreen === 'undefined' || typeof theGameScreen.playerTank === 'undefined' || !theGameScreen.playerTank ){
+    try{
+        let relativeXPosition = event.clientX - $('#mainScreen').offset().left;
+        let relativeYPosition = event.clientY - $('#mainScreen').offset().top;
+        theGameScreen.playerTank.alignTurret( relativeXPosition, relativeYPosition );
+    }
+    catch{
         return;
     }
-    let relativeXPosition = event.clientX - $('#mainScreen').offset().left;
-    let relativeYPosition = event.clientY - $('#mainScreen').offset().top;
-    theGameScreen.playerTank.alignTurret( relativeXPosition, relativeYPosition );
 }
-
 function changeScreens( event ){
     event.stopPropagation()
     $( '#startScreen' ).addClass( 'hidden' );
